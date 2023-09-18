f171fa22f331b80822b8d04b55da2bebdcb4b55d
mayapony <mayapony@foxmail.com>
2023-09-18 17:24:08 +0800

    🎉 第一次提交

---
+++ components/Footer.vue
@@ -0,0 +1,13 @@
+<template>
+  <BarBottom  title="安徽大学" class="slide-footer">
+    <Item text="mayapony">
+      <carbon:logo-github />
+    </Item>
+    <Item text="mayapony.site">
+      <carbon:link />
+    </Item>
+    <Item text="马书豪">
+      <carbon:user-avatar-filled />
+    </Item>
+  </BarBottom>
+</template>
---
+++ uno.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from "unocss";
+
+export default defineConfig({
+  shortcuts: {
+    "slide-footer": "bg-[#dce0e8] text-[#4c4f69]",
+    "slide-main": "bg-[#eff1f5] text-[#4c4f69]",
+  },
+});
